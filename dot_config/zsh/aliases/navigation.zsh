1dbe193749df8780bc895fe9902129fe2b71cd5a
peppapig450 <peppapig450@pm.me>
2025-06-04 13:48:10 -0700

    refactor(zsh): move dir navigation aliases to aliases/navigation.zsh
    Moved directory navigation aliases (..., ...., etc.) out of
    dot_zsh_aliases into a dedicated aliases/navigation.zsh for better
    organization. This change sets the stage for refactoring our zsh aliases
    into a dedicated directory for better modularity and future support for
    lazy loading. No functional changes to alias behavior.

---
+++ dot_config/zsh/aliases/navigation.zsh
@@ -0,0 +1,6 @@
+# navigation.zsh
+# Aliases for quickly navigating back multiple directories in the filesystem.
+alias ...=../.
+alias ....=../../.
+alias .....=../../../.
+alias ......=../../../../.
